55e991378459ab92a57ebabd32dbc2a71da8eff2
hagarashi8 <Artem501137@gmail.com>
2023-04-10 17:31:07 +0000

    Make main seperate function

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import parser
 
-if __name__ == "__main__":
+def main():
     driver = parser.get_driver()
     parser.get_page(driver, "https://omsk.hh.ru/search/vacancy?text=Python&area=68")
     jobs = parser.get_job_list(driver)
     parser.df_and_save(jobs, "jobs.xlsx")
+
+if __name__ == "__main__":
+    main()
